8e492af4d512887055055cb7ba255a5e389e78f0
onesimusa1 <onesimusa1@gmail.com>
2022-09-13 18:35:47 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
+
+
 /**
- *
- *  * print_alphabet - This function prints the alphabet in lowercase
- *
- *   * followed by a new line
- *
- *    */
+
+ * print_alphabet - This function prints the alphabet in lowercase
+
+ * followed by a new line
+
+ */
 
 void print_alphabet(void)
 
@@ -14,14 +16,19 @@ void print_alphabet(void)
 
 	int ch;
 
+
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 
 	{
 
-        	_putchar(ch);
+		_putchar(ch);
 
 	}
 
+
+
 	_putchar('\n');
 
-}
+
+

dd0cff5232281081314931adb4a2da688888621f
onesimusa1 <onesimusa1@gmail.com>
2022-09-13 17:02:34 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,37 @@
+#include <unistd.h>
+
 #include "main.h"
+
 /**
+
+ * _putchar - writes the character c to the stdout
+
+ * @c: The character to print
+
+ * main - Code entry
+
  *
- *  * main - Code entry
+
+ * Return: On success 1.
+
+ * On error, -1 is returned, and errno is set appropriately.
+
+ * Description: This program prints _putchar
+
  *
- *  *
- *
- *  * Description: This program prints _putchar
- *
- *  *
- *
- *  * Return: 0
- *
- *  */
+
+ * Return: 0
+
+ */
+
+ int _putchar(char c)
 
 int main(void)
 
 {
+
+	return (write(1, &c, 1));
+
 	_putchar('_');
 
 	_putchar('p');
@@ -30,9 +46,9 @@ int main(void)
 
 	_putchar('a');
 
-	_putchar('r');
+	_putchar('a');
 
 	_putchar('\n');
+
 	return (0);
 
-}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _main_h_
+
+#define _main_h_
+
+
+
+int _putchar(char c);
+
+
+
+#endif

d98a9166accff3257c98dd0e8d0f397388b8e538
onesimusa1 <onesimusa1@gmail.com>
2022-09-14 07:57:27 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	int count = 0;
+
+	char let;
+
+
+
+	while (count++ <= 9)
+
+	{
+
+		for (let = 'a'; let <= 'z'; let++)
+
+			_putchar(let);
+
+		_putchar('\n');
+
+	}
+
+}

13ec4ddb4ea74a5d4a06dc6b1b29bcdc7ca64fd9
onesimusa1 <onesimusa1@gmail.com>
2022-09-14 20:57:49 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,13 @@ void jack_bauer(void)
 	for (hour = 0; hour <= 23; hour++)
 	{	
 		for (minute = 0; minute <= 59; minute++)						
-		{								
-	 		_putchar((hour / 10) + '0');					
+		{
+			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
-			_putchar(':');									
-			_putchar((minute / 10) + '0');					
-			_putchar((minute % 10) + '0');						
-			_putchar('\n');																											}
+	 	        _putchar(':');	
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');	
+			_putchar('\n');
+		}
 	}
 }

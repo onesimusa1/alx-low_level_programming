04bd39f70616351f4f061ef26ab34363795ee857
onesimusa1 <onesimusa1@gmail.com>
2022-09-15 03:10:42 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0')
+		_putchar('0');
 
 			for (mult = 1; mult <= 9; mult++)
 			{
@@ -20,6 +20,6 @@ void times_table(void)
 																			    _putchar((prod / 10) + '0');
 																		    _putchar((prod % 10) + '0');
 			}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }

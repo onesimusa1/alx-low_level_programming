439d44b5bfed38f43136f46f79c11c9f86039447
onesimusa1 <onesimusa1@gmail.com>
2022-09-19 07:28:42 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to to 9          8 
+ *   @n ; This is an empty entry
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	int n;
+
+	n=98;
+
+	return = (0);
+}

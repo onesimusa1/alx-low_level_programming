694b15e46cf01f67ca1387d37fc4e3b5650836fe
onesimusa1 <onesimusa1@gmail.com>
2022-09-19 15:28:23 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev -> Printing a string in reverse
+ * @s: the string to be printed in rev
+ */
+
+void print _rev(char *s)
+{
+	ini i, n;
+	
+	n = strlen(s);
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+}
+

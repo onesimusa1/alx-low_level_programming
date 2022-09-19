3812e470ad5703db436382706dc7670922633710
onesimusa1 <onesimusa1@gmail.com>
2022-09-19 13:14:59 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int -> give that two integers swap the values they are holding
+ * @a: parameter 1
+ * @b parameter 2
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	a = 47, b = 68
+		tmp = *a;
+		*a = *b;
+		b* = tmp;
+}
